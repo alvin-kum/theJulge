cd44ff9ab7acb5074f8db3041332f3124b5b10e9
hyeonyeongji <gusdud227@gmail.com>
2025-09-11 22:20:29 +0900

    Update transformers.ts
    shop.id 추가

--- src/utils/transformers.ts
+++ src/utils/transformers.ts
@@ -42,11 +42,7 @@ export type NoticeItem = {
 export const transformNoticeToPostData = (notice: NoticeItem): PostData => {
   return {
     id: notice.id,
-<<<<<<< HEAD
-    shopId:notice.shop.item.id,
-=======
     shopId: notice.shop.item.id,
->>>>>>> 7aedd14ff6b3c64f2262bd8877c014548467abb4
     name: notice.name ?? notice.shop.item.name,
     imageUrl: notice.imageUrl ?? notice.shop.item.imageUrl,
     address1: notice.address1 ?? notice.shop.item.address1,
