732a9dc4096ec7fab20fc573377f8a960d9855f3
WEESUNGJIN <wsj9169@gmail.com>
2025-09-11 18:39:34 +0900

    공고 테이블 스타일 수정

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -13,7 +13,7 @@ import {
   CloseButton,
 } from "./profileEdit.styles";
 
-import Modal from "@/components/Modal";
+import Modal from "@/components/Modal/Modal";
 import { updateMyProfile, fetchMyInfo } from "../../lib/api/user";
 
 export default function ProfileEditPage() {
