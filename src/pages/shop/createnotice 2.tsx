0d1113a4aa59265db004aa0583ac08b9ecbbb8a1
kum.jh <kjh0650@gmail.com>
2025-09-12 11:56:18 +0900

    프로젝트 최종 메인

--- src/pages/shop/createnotice 2.tsx
+++
@@ -1,9 +0,0 @@
-import NoticeCreateForm from "@/components/notice/NoticeCreateForm";
-
-export default function CreateNoticePage() {
-  return (
-    <>
-      <NoticeCreateForm />
-    </>
-  );
-}
